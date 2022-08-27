8386637c0e75daa339253f78c12259f85eedc29e
Nikolay-Akymenko <nikolay0939@gmail.com>
2022-08-27 19:34:09 +0300

    Добавляет модалку

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
+
+
 // Change code below this line
 
-console.log(galleryItems);
+//*<div class="gallery"></div>
+
+const galleryContainer = document.querySelector('.gallery');
+const imgMarkup = createImgMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+// console.log(createImgMarkup(galleryItems));
+
+function createImgMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+             />
+         </a>
+    </div>
+    `;
+    })
+    .join('');
+}
+
+function onGalleryContainerClick(event) {
+    console.log(event.target);
+}
+
+
+
