a9c2b4ed3e783a0a9b10a0c87bef9bda4b87315a
Nikolay-Akymenko <nikolay0939@gmail.com>
2022-08-28 22:44:33 +0300

    bugfix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -12,13 +12,13 @@ galleryContainer.insertAdjacentHTML("beforeend", imgMarkup);
 
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 
-// console.log(createImgMarkup(galleryItems));
+const modalInstance = getModal();
 
 function createImgMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
     <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
+        <a class="gallery__link" href="large-image.jpg">
             <img
                 class="gallery__image"
                 src="${preview}"
@@ -33,8 +33,31 @@ function createImgMarkup(galleryItems) {
 }
 
 function onGalleryContainerClick(event) {
-    console.log(event.target);
-}
+    event.preventDefault();
 
+    if (!event.classList.contains('.gallery__item'))
+        return;
+
+    const currentImageUrl = event.target.dataset.source;
+    instance = basicLighbox.create(
+        `<img class="modal__image" src"${currentImageUrl}" />`
+    );
+    instance.show();
+}
+function onModalOpen(event) {
+    window.addEventListener('keydown', onModalClose);
+}
+function onModalClose(event) {
+    window.addEventListener('keydown', onKeyPress);
+}
+function onKeyPress(event) {
+    const ESK_KEY_CODE = 'Escape';
+    const isKeyCode = event.code === ESK_KEY_CODE;
+
+    if (isKeyCode) {
+        instance.close();
+        window.removeEventListener('keydown', onModalClose);
+    }
+}
 
 
